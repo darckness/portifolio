0ebd27d05f377e441024b0ef3cc4573c0fdd5514
Joao Marcos <joaomarcoslopes48@gmail.com>
2024-07-04 23:22:05 -0300

    chore: new colors and animation time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
    <div className="bg-gray-900 ">
     <div className="bg-gray-700 w-1/2 h-screen">
-    <div className="rounded-full h-2/4 w-10 bg-gray-800 absolute top-1/4 left-1/2 transform -translate-x-1/2 transition-transform-color-border duration-1000 hover:border hover:scale-150 hover:bg-gray-400">
+    <div className="rounded-full border h-2/4 w-10 bg-gray-800 absolute top-1/4 left-1/2 transform -translate-x-1/2 transition-transform-color-border duration-1000 hover:scale-150 hover:bg-gray-400">
     </div>
     </div>
    </div>
