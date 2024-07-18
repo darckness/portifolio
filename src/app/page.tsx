40e8fbea503bbd5dc9efa21a3b15c3add41a9158
Joao Marcos <joaomarcoslopes48@gmail.com>
2024-07-18 08:18:25 -0300

    fix: Ajust code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
    <div className="bg-gray-900 ">
     <div className="bg-gray-700 w-1/2 h-screen">
-    <div className="rounded-full border h-2/4 w-10 bg-gray-800 absolute top-1/4 left-1/2 transform -translate-x-1/2 transition-transform-color border duration-1000 hover:scale-150 hover:bg-gray-400">
+      <h1 className="flex absolute top-1/3 left-1/4 -translate-x-1/2">Olá meu nome é João Marcos</h1>
+    <div className="rounded-full h-2/4 w-10 bg-gray-800 absolute top-1/4 left-1/2 transform -translate-x-1/2 transition-transform-color border duration-1000 hover:scale-150 hover:bg-gray-400 hover:w-20">
     </div>
     </div>
    </div>
