1d493b43c1febb79107dd3037b4351bf8ea8cc31
Joao Marcos <joaomarcoslopes48@gmail.com>
2024-07-23 22:54:54 -0300

    feat: New text and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
    <div className="bg-gray-900 ">
     <div className="bg-gray-700 w-1/2 h-screen">
       <h1 className="flex absolute top-56 left-1/4 -translate-x-56 text-white font-Bungee text-9xl transition-transform-color duration-1000 hover:text-gray-400">Olá</h1>
-      <h1 className="flex absolute top-96 left-1/4 -translate-x-56 text-yellow-500 font-Anton text-6xl transition-transform-color-text duration-1000 hover:text-yellow-400 hover:text-7xl">Me chamo João Lopes</h1>
-      <p className="flex absolute  top-2/4 left-1/4 -translate-x-56 font-Anton text-4xl leading-normal text-white transition-transform-color duration-1000 hover:text-yellow-500 hover:text-5xl hover:leading-loose">
+      <h1 className="flex absolute top-96 left-1/4 -translate-x-56 text-yellow-500 font-Anton text-6xl transition-transform-color duration-1000 hover:text-yellow-400">Me chamo João Lopes</h1>
+      <p className="flex absolute  top-3/4 left-1/4 -translate-x-56 font-Anton text-4xl leading-normal text-white transition-transform-color duration-1000 hover:text-yellow-500">
       Seja bem-vindo!<br />
       Dê uma olhada nos meus projetos<br />
       e descubra como posso contribuir<br />
